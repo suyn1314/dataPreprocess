92de76e15f5e72f14ef5dc336b05b104efba0787
Enor <suyn1314@gmail.com>
2022-08-30 17:50:21 +0800

    .

--- JsontoCsv.py
+++ JsontoCsv.py
@@ -6,7 +6,6 @@ Created on Mon Aug 22 18:24:06 2022
 """
 import json
 import csv
-from decimal import Decimal
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,18 +14,20 @@ import matplotlib.pyplot as plt
 # into the variable data
 
 
-coolkw_list = []
-time_list = []
-count = -1
-with open('datasets/2020/0013157800087578_08.json',encoding='utf-8') as f:
+cop_right_list = []
+cop_left_list = []
+cop_right_time = []
+cop_left_time = []
+countR = -1
+countL = -1
+with open('datasets/2020/0013157800087578_09.json',encoding='utf-8') as f:
     while True:
         
         line = f.readline()
         if not line: # 到 EOF，返回空字符串，则终止循环
             break
-        count +=1
+        
         json_array = json.loads(line)
-
         icewaterin  = float(json_array["icewaterin"])
         icewaterout = float(json_array["icewaterout"])
         kw_right = float(json_array["kw_r"])
@@ -52,11 +53,26 @@ with open('datasets/2020/0013157800087578_08.json',encoding='utf-8') as f:
         """
         
         if  kw_right > 0:
-            coolkw_list.insert(count, coolkw)
-            time_list.insert(count, count)
-        elif kw_left > 0:
-            coolkw_list.insert(count, coolkw)
-            time_list.insert(count, count)
+            countR +=1
+            cop_right = coolkw / kw_right
+            if 0 < cop_right < 4.3 :
+                cop_right_list.insert(countR, cop_right)
+                cop_right_time.insert(countR, countR)
+                print("日期:" + json_array["timestamp"])
+                print(f'冷凍能力 kw: {coolkw} Kw')
+                print("右耗電(KW): " + json_array["kw_r"] + "KW")
+                print(f'右機COP: {cop_right}')
+        if  kw_left > 0:
+            countL +=1
+            cop_left = coolkw / kw_left
+            if 0 < cop_left < 4.3:
+                cop_left_list.insert(countL, cop_left)
+                cop_left_time.insert(countL, countL)
+                print("日期:" + json_array["timestamp"])
+                print(f'冷凍能力 kw: {coolkw} Kw')
+                print("左耗電(KW):" + json_array["kw_l"] + "KW")
+                print(f'左機COP: {cop_left}')
+        
         
         
         #t1 = 開始時間
@@ -72,11 +88,10 @@ with open('datasets/2020/0013157800087578_08.json',encoding='utf-8') as f:
         #COP = 製冷能力(KW) / 消耗電力(KW) = 蒸發器入出口焓差 / 壓縮機出入口焓差
         #COP = ((t2h1 - t1h1) - (t2h3 - t1h3)) / ((t2h2 - t1h2) - (t2h1 - t1h1)) = Tc / (Th - Tc)
         
-      
-        print("日期:" + json_array["timestamp"])
-        print(f'冷凍能力 kw: {coolkw} Kw')
         
         """
+        print("日期:" + json_array["timestamp"])
+        print(f'冷凍能力 kw: {coolkw} Kw')
         print("冷卻水入口溫度:" + json_array["coolwaterin"] + "℃")
         print("冷卻水出口溫度:" + json_array["coolwaterout"] + "℃")
         print("冰水入口溫度:" + json_array["icewaterin"] + "℃")
@@ -94,13 +109,21 @@ with open('datasets/2020/0013157800087578_08.json',encoding='utf-8') as f:
 #print(coolkw_list)
 #print(time_list)
 
-fig3 = plt.figure(figsize=(40,10),dpi=500)       
-plt.title("") # title
-plt.ylabel("Coolpower") # y label
+fig1 = plt.figure(figsize=(40,10),dpi=500)       
+plt.title("Right COP") # title
+plt.ylabel("COP") # y label
 plt.xlabel("Time/number") # x label
-plt.plot(time_list,coolkw_list)
-plt.savefig("2020Data8.png")
-plt.show(fig3)
+plt.plot(cop_right_time,cop_right_list)
+plt.savefig("2020DataRight9.png")
+plt.show(fig1)
+
+fig2 = plt.figure(figsize=(40,10),dpi=500)       
+plt.title("Left COP") # title
+plt.ylabel("COP") # y label
+plt.xlabel("Time/number") # x label
+plt.plot(cop_left_time,cop_left_list)
+plt.savefig("2020DataLeft9.png")
+plt.show(fig2)
 
 
 
