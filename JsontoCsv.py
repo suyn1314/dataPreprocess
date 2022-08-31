6ed9cfe8d71a63c8eeab0d17abf2778c8004db5d
Enor <suyn1314@gmail.com>
2022-08-31 15:46:40 +0800

    .

--- JsontoCsv.py
+++ JsontoCsv.py
@@ -7,17 +7,24 @@ Created on Mon Aug 22 18:24:06 2022
 import json
 import csv
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import interpolate
+#須安裝: pip install tsmoothie
+from tsmoothie.utils_func import sim_randomwalk
+from tsmoothie.smoother import LowessSmoother
 
 # JSON file to CSV
 # Opening JSON file and loading the data
 # into the variable data
 
 
-cop_right_list = []
+cop_right_list = [] 
 cop_left_list = []
-cop_right_time = []
-cop_left_time = []
+cop_right_number = []
+cop_left_number = []
+cop_right_date = []
+cop_left_date = []
 countR = -1
 countL = -1
 with open('datasets/2020/0013157800087578_09.json',encoding='utf-8') as f:
@@ -45,32 +52,36 @@ with open('datasets/2020/0013157800087578_09.json',encoding='utf-8') as f:
          右機 1200L/min
         """
         # 冷凍能力(kw) = (流量 * 進出水溫差(C) * 60(h/m)) / 860
-        coolkw = round((( 1300 * (icewaterin - icewaterout) * 60) / 860),2)
+        coolkw = (( 1300 * (icewaterin - icewaterout) * 60) / 860)
         
         """
         coolkw_list.insert(count, coolkw)
         time_list.insert(count, count)
         """
-        
+        #如果右機有電流
         if  kw_right > 0:
             countR +=1
             cop_right = coolkw / kw_right
             if 0 < cop_right < 4.3 :
                 cop_right_list.insert(countR, cop_right)
-                cop_right_time.insert(countR, countR)
-                print("日期:" + json_array["timestamp"])
+                cop_right_number.insert(countR, countR)
+                cop_right_date.insert(countR, time)
+                print("日期:" + time)
                 print(f'冷凍能力 kw: {coolkw} Kw')
-                print("右耗電(KW): " + json_array["kw_r"] + "KW")
+                print(f'右耗電(KW): {kw_right} KW')
                 print(f'右機COP: {cop_right}')
+        
+        #如果左機有電流        
         if  kw_left > 0:
             countL +=1
             cop_left = coolkw / kw_left
             if 0 < cop_left < 4.3:
                 cop_left_list.insert(countL, cop_left)
-                cop_left_time.insert(countL, countL)
-                print("日期:" + json_array["timestamp"])
+                cop_left_number.insert(countL, countL)
+                cop_left_date.insert(countL, time)
+                print("日期:" + time)
                 print(f'冷凍能力 kw: {coolkw} Kw')
-                print("左耗電(KW):" + json_array["kw_l"] + "KW")
+                print(f'左耗電(KW): {kw_left} KW')
                 print(f'左機COP: {cop_left}')
         
         
@@ -104,28 +115,75 @@ with open('datasets/2020/0013157800087578_09.json',encoding='utf-8') as f:
         print("右耗電(KW): " + json_array["kw_r"] + "KW")
         """
         
-        
-        
-#print(coolkw_list)
+#print(cop_left_date)
 #print(time_list)
+#平滑處理(scipy)
+funR = interpolate.interp1d(cop_right_number,cop_right_list,kind='cubic')
+sR = funR(cop_right_number)
+#平滑處理(Tsmoothie)
+tsmoothieR = LowessSmoother(smooth_fraction = 0.02,iterations=1)
+tsmoothieR.smooth(cop_right_list)
+tR = tsmoothieR.smooth_data #東西存在.smooth_data 裡面
+COPr = [float(x) for item in tR for x in item ] #刪除np.ndarray中的括號
 
-fig1 = plt.figure(figsize=(40,10),dpi=500)       
+tsmoothieL = LowessSmoother(smooth_fraction = 0.02,iterations=1)
+tsmoothieL.smooth(cop_left_list)
+tL = tsmoothieL.smooth_data #東西存在.smooth_data 裡面
+COPl = [float(x) for item in tL for x in item ] #刪除np.ndarray中的括號
+#print(np.ndarray.tolist(tR))
+#print(COPr)
+
+#右機畫圖
+fig1 = plt.figure(figsize=(10,5),dpi=200)       
 plt.title("Right COP") # title
 plt.ylabel("COP") # y label
 plt.xlabel("Time/number") # x label
-plt.plot(cop_right_time,cop_right_list)
-plt.savefig("2020DataRight9.png")
+plt.plot(cop_right_number,COPr)#(x,y)
+plt.ylim([0, 5])
+plt.savefig("img/2020DataRight9.png")
 plt.show(fig1)
 
-fig2 = plt.figure(figsize=(40,10),dpi=500)       
+#左機畫圖
+fig2 = plt.figure(figsize=(10,5),dpi=200)       
 plt.title("Left COP") # title
 plt.ylabel("COP") # y label
 plt.xlabel("Time/number") # x label
-plt.plot(cop_left_time,cop_left_list)
-plt.savefig("2020DataLeft9.png")
+plt.plot(cop_left_number,COPl)
+plt.ylim([0, 5])
+plt.savefig("img/2020DataLeft9.png")
 plt.show(fig2)
 
+#原始數據(右)
+fig3 = plt.figure(figsize=(10,5),dpi=200)       
+plt.title("Orgin Right COP") # title
+plt.ylabel("COP") # y label
+plt.xlabel("Time/number") # x label
+plt.plot(cop_right_number,cop_right_list)#(x,y)
+plt.ylim([0, 5])
+plt.savefig("img/2020DataOrginRight9.png")
+plt.show(fig3)
 
+#原始數據(左)
+fig4 = plt.figure(figsize=(10,5),dpi=200)       
+plt.title("Orgin Left COP") # title
+plt.ylabel("COP") # y label
+plt.xlabel("Time/number") # x label
+plt.plot(cop_left_number,cop_left_list)
+plt.ylim([0, 5])
+plt.savefig("img/2020DataOrginLeft9.png")
+plt.show(fig4)
+
+#右機存檔
+rightDictionary = {"time" : cop_right_date ,
+                   "C.O.P" : COPr }
+rightCOP=pd.DataFrame(data=rightDictionary)
+rightCOP.to_csv('datasets/copData/2020copR9.csv',encoding='utf-8')
+
+#左機存檔
+leftDictionary = {"time" : cop_left_date ,
+                  "C.O.P" : COPl }
+leftCOP=pd.DataFrame(data=leftDictionary)
+leftCOP.to_csv('datasets/copData/2020copL9.csv',encoding='utf-8')
 
 
 
